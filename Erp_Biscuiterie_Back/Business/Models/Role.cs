ff588b23fd1dd8f3967374c654bdc9ea204d7ae2
Phil France <phil-france.martinez@campusid.eu>
2019-03-21 17:11:56 +0100

    [BACK-O00] : Added all models

--- Erp_Biscuiterie_Back/Business/Models/Role.cs
+++ Erp_Biscuiterie_Back/Business/Models/Role.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Erp_Biscuiterie_Back.Business.Models
 {
-    public class Role
+    public partial class Role
     {
+        public Role()
+        {
+            User = new HashSet<User>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
 
--- Erp_Biscuiterie_Back/Business/Models/User.cs
+++ Erp_Biscuiterie_Back/Business/Models/User.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Erp_Biscuiterie_Back.Business.Models
 {
-    public class User
+    public partial class User
     {
         public int Id { get; set; }
         public string Lastname { get; set; }
         public string Firstname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
         public int RoleId { get; set; }
+
         public Role Role { get; set; }
     }
 }
