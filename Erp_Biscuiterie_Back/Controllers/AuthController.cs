41decd9ecca3647c21cb481a9691a015f5e7ac6e
Davide <davide.gagliardi@yahoo.it>
2019-04-03 21:28:45 +0200

    mot de passe crypté (jwt token / helpers)

---
+++ Erp_Biscuiterie_Back/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Erp_Biscuiterie_Back.Business.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Erp_Biscuiterie_Back.Utils.Helpers;
+
+namespace Erp_Biscuiterie_Back.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token/{roleId}")]
+        public ActionResult GetToken(int roleId)
+        {
+
+            //Security key for sign token and validate it later
+            string securityKey = GlobalValue.SecretKey;
+
+            //Symmetric security key
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            //Signing credentials
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //Take Right Value for roleid
+            var role = ExternalSqlActions.Instance.GetRole(roleId);
+
+
+            var claim = new List<Claim>();
+            claim.Add(new Claim(ClaimTypes.Role, role));
+
+            //Create token
+            var token = new JwtSecurityToken(
+                    issuer: GlobalValue.Issuer,
+                    audience: GlobalValue.Audience,
+                    expires: DateTime.Now.AddMinutes(20),
+                    signingCredentials: signingCredentials,
+                    claims: claim
+                );
+
+            //return token
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+
+        }
+
+        // GET api/values
+        /*[HttpGet]
+        [Authorize(Roles ="Administrator")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }*/
+    }
+}
---
+++ Erp_Biscuiterie_Back/Utils/Helpers/GlobalValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Erp_Biscuiterie_Back.Utils.Helpers
+{
+
+    public static class GlobalValue
+    {
+        //Can't modify this file
+        public const String Issuer = "cmp.id";
+        public const String Audience = "users";
+        public const String SecretKey = "this_is_campus_id_secret";
+    }
+}
