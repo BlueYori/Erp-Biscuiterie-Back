b207d6823919248124b55e3a63bf254c0163cef4
Unknown <2013189@campusid.eu>
2019-06-19 15:34:36 +0200

    [Develop] : Added include orderDetails in OrderController

--- Erp_Biscuiterie_Back/Controllers/OrderController.cs
+++ Erp_Biscuiterie_Back/Controllers/OrderController.cs
@@ -25,7 +25,7 @@ namespace Erp_Biscuiterie_Back.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Order>>> GetOrder()
         {
-            return await _context.Order.ToListAsync();
+            return await _context.Order.Include(order => order.OrderDetails).ToListAsync();
         }
 
         // GET: api/Order/5
